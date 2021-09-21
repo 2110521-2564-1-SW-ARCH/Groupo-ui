42ab27ab2be9b01a7f67f4651de2df50674a2b29
Sornram Saetang <waterfishX@outlook.com>
2021-09-21 19:46:06 +0700

    set up page

---
+++ src/components/SignUpForm/index.tsx
@@ -0,0 +1,135 @@
+import React from "react";
+import {
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Link,
+  TextField
+} from "@mui/material";
+import { Box } from "@mui/system";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const INITIAL_VALUE = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  agreement: false
+};
+
+const validationSchema = yup.object({
+  firstName: yup.string().required("First name is required"),
+  lastName: yup.string().required("Last name is required"),
+  email: yup
+    .string()
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(8, "Password should be of minimum 8 characters length")
+    .required("Password is required")
+});
+
+const SignUpForm = () => {
+  const formik = useFormik({
+    initialValues: INITIAL_VALUE,
+    validationSchema: validationSchema,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
+
+  return (
+    <Box component="form" onSubmit={formik.handleSubmit} sx={{ mt: 3 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            autoComplete="fname"
+            name="firstName"
+            fullWidth
+            value={formik.values.firstName}
+            onChange={formik.handleChange}
+            error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+            helperText={formik.touched.firstName && formik.errors.firstName}
+            id="firstName"
+            label="First Name"
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            id="lastName"
+            label="Last Name"
+            name="lastName"
+            autoComplete="lname"
+            value={formik.values.lastName}
+            onChange={formik.handleChange}
+            error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+            helperText={formik.touched.lastName && formik.errors.lastName}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+            autoComplete="email"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="new-password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                color="primary"
+                name="agreement"
+                value={formik.values.agreement}
+                onChange={formik.handleChange}
+              />
+            }
+            label="I agree to the terms and services."
+          />
+        </Grid>
+      </Grid>
+      <Button
+        disabled={formik.isSubmitting || !formik.values.agreement}
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+      >
+        Sign Up
+      </Button>
+      <Grid container justifyContent="flex-end">
+        <Grid item>
+          <Link href="#" variant="body2">
+            Already have an account? Sign in
+          </Link>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default SignUpForm;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import SignUpPage from "./pages/SignUpPage";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme();
+
+const RoutedApp = () => (
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Route path="/" component={SignUpPage} />
+    </BrowserRouter>
+  </ThemeProvider>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RoutedApp />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
