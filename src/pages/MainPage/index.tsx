2b6eb9f9c7197f7ca888772949e5c82ec944a660
Sornram Saetang <waterfishX@outlook.com>
2021-09-25 16:56:00 +0700

    Add nav, profile page

---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,8 @@
+import {  Container } from "@mui/material";
+import Nav from "../../components/Nav";
+
+const MainPage = () => {
+    return (<Nav />)
+};
+
+export default MainPage;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,19 @@
 import { Route, Switch } from "react-router";
+import NavRoute from "../components/NavRoute";
 import Login from "../pages/LoginPage";
+import MainPage from "../pages/MainPage";
+import ProfilePage from "../pages/ProfilePage";
 import SignUp from "../pages/SignUpPage";
 
 const Routes = () => {
-    return <Switch>
-        <Route exact path="/" component={SignUp} />
-        <Route path="/login" component={Login}/>
-    </Switch>;
+  return (
+    <Switch>
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/login" component={Login} />
+      <NavRoute exact path="/profile" component={ProfilePage} />
+      <Route path="/" component={MainPage} />
+    </Switch>
+  );
 };
 
 export default Routes;
