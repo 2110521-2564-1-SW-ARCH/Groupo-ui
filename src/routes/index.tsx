e1659764bd3c0a1e970fc5578725f17480b30a99
Kiattipoom Rojvanakarn <kiattipoom.r@gmail.com>
2021-09-26 19:28:07 +0700

    Wrapped most pages with auth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Route, Switch } from "react-router";
 import NavRoute from "../components/NavRoute";
+import RequireAuth from "../components/RequireAuth";
 import BoardListPage from "../pages/BoardListPage";
 import CreateBoardPage from "../pages/CreateBoardPage";
 import JoinPage from "../pages/JoinPage";
@@ -12,10 +13,18 @@ const Routes = () => {
     <Switch>
       <Route exact path="/signup" component={SignUp} />
       <Route exact path="/login" component={Login} />
-      <NavRoute exact path="/profile" component={ProfilePage} />
-      <NavRoute exact path="/create" component={CreateBoardPage} />
-      <NavRoute exact path="/join" component={JoinPage} />
-      <NavRoute exact path="/" component={BoardListPage} />
+      <NavRoute exact path="/profile" component={() => (
+        <RequireAuth component={ProfilePage} />
+      )} />
+      <NavRoute exact path="/create" component={() => (
+        <RequireAuth component={CreateBoardPage} />
+      )} />
+      <NavRoute exact path="/join" component={() => (
+        <RequireAuth component={JoinPage} />
+      )} />
+      <NavRoute exact path="/" component={() => (
+        <RequireAuth Component={BoardListPage} />
+      )} />
     </Switch>
   );
 };
