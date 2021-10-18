f77630c40854b538bd95ec9b1747bb46f316041f
Sornram Saetang <waterfishX@outlook.com>
2021-10-18 11:59:49 +0700

    grouping-board

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import NavRoute from "../components/NavRoute";
 import RequireAuth from "../components/RequireAuth";
 import BoardListPage from "../pages/BoardListPage";
 import CreateBoardPage from "../pages/CreateBoardPage";
+import GroupPage from "../pages/GroupPage";
 import JoinPage from "../pages/JoinPage";
 import Login from "../pages/LoginPage";
 import ProfilePage from "../pages/ProfilePage";
@@ -13,18 +14,27 @@ const Routes = () => {
     <Switch>
       <Route exact path="/signup" component={SignUp} />
       <Route exact path="/login" component={Login} />
-      <NavRoute exact path="/profile" component={() => (
-        <RequireAuth Component={ProfilePage} />
-      )} />
-      <NavRoute exact path="/create" component={() => (
-        <RequireAuth Component={CreateBoardPage} />
-      )} />
-      <NavRoute exact path="/join" component={() => (
-        <RequireAuth Component={JoinPage} />
-      )} />
-      <NavRoute exact path="/" component={() => (
-        <RequireAuth Component={BoardListPage} />
-      )} />
+      <NavRoute
+        exact
+        path="/profile"
+        component={() => <RequireAuth Component={ProfilePage} />}
+      />
+      <NavRoute
+        exact
+        path="/create"
+        component={() => <RequireAuth Component={CreateBoardPage} />}
+      />
+      <NavRoute
+        exact
+        path="/join"
+        component={() => <RequireAuth Component={JoinPage} />}
+      />
+      <NavRoute
+        exact
+        path="/"
+        component={() => <RequireAuth Component={BoardListPage} />}
+      />
+      <NavRoute exact path="/group/:id" component={GroupPage} />
     </Switch>
   );
 };
