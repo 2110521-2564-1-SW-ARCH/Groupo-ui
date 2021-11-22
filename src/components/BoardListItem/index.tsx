92d66699a7a6ec0bc1eeb24d72d7b08a7c3f676a
Vee46834 <Veerawong46834@hotmail.co.th>
2021-11-22 14:10:52 +0700

    add exit and delete button1

--- src/components/BoardListItem/index.tsx
+++ src/components/BoardListItem/index.tsx
@@ -2,6 +2,8 @@ import { Box, Typography, Grid } from "@mui/material";
 import GroupsIcon from "@mui/icons-material/Groups";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 import { Link } from "react-router-dom";
+import { Delete as DeleteIcon, ExitToApp } from "@mui/icons-material";
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 import "./style.css";
 import { BoardItem } from "../../models/type/board";
@@ -23,16 +25,19 @@ const BoardListItem = ({board}: {board: BoardItem, user: string|undefined}) => {
           <Typography variant="h5">{board.name}</Typography>
         </Link>
         <Grid container spacing={1}>
-          <Grid item xs={5} display="flex" gap={1} color="gray" fontSize="10px">
+          <Grid item xs={4} display="flex" gap={1} color="gray" fontSize="10px">
             <GroupsIcon />
             <Typography>{board.totalGroup} groups</Typography>
           </Grid>
-          <Grid item xs={5} display="flex" gap={1} color="gray" fontSize="10px">
+          <Grid item xs={4} display="flex" gap={1} color="gray" fontSize="10px">
             <PermIdentityIcon />
             <Typography>{board.totalMember} joined</Typography>
           </Grid>
-          <Grid item xs={2} display="flex" gap={1} color="gray" fontSize="10px">
-            <PermIdentityIcon />
+          <Grid item xs={2} display="flex" gap={1}>
+            <DeleteIcon/>
+          </Grid>
+          <Grid item xs={2} display="flex">
+            <ExitToAppIcon/>
           </Grid>
         </Grid>
       </Box>
